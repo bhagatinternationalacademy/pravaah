febeeeabe4e4b8ebd0e1e8ef580431978cda814d
ankita01209 <ankitashanbhag25@gmail.com>
2026-05-28 15:20:27 +0530

    Updated dashboard module

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
+
+class DashboardCard(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    total_count = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Notification(models.Model):
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.message[:50]
+
+
+class ActivityLog(models.Model):
+    user_name = models.CharField(max_length=100)
+    action = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user_name} - {self.action}"
 # Create your models here.
